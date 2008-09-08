54bccae5a3905ab50db678ef0201c4345662e168
paularms.com <paularms.com@6bdbecb3-7c55-0410-a0e7-a0c93fd440fa>
2008-09-08 21:51:36 +0000

    big cleanup on js. added Object.addMethods, Object.extend
    git-svn-id: svn+ssh://paularms.com/home/989/data/svn/jsdev/trunk@8 6bdbecb3-7c55-0410-a0e7-a0c93fd440fa

--- slim.js
+++ slim.js
@@ -1,122 +1,64 @@
-Element = function(type, atts) {
-    this.el = document.createElement(type);
-
-    for(attr in atts) {
-        this.el.setAttribute(attr, atts[attr]);
-    }
-    
-    return this.el;
-};
-
-Function.prototype.bindAsEventListener = function() {
-    var __method = this, object = arguments[0];
-    return function(event) {
-      return __method.apply(object, [event || window.event].concat(arguments));
+// super simple CSS selector. select only by ID or single class name
+var $ = function(selector) {
+    if(typeof document.querySelectorAll !== 'function') {
+        return document.querySelectorAll(selector);
+    } else {
+        if(selector.indexOf('#') == 0) {
+            return [document.getElementById(selector.replace('#', ''))];
+        } else if(selector.indexOf('.') == 0) {
+            return document.getElementsByClassName(selector.replace(/\./, ''));
+        }
     }
 };
 
-Function.prototype.bind = function() {
-    if (arguments.length < 2 && !arguments[0]) return this;
-    var __method = this,  object = arguments[0];
-    return function() {
-      return __method.apply(object, arguments);
-    }
-}
+Object.extend = function(destination, source) {
+  for(var property in source) {
+      destination[property] = source[property];
+  }
+  return destination;
+};
 
-HTMLElement.prototype.setStyle = function(styles) {
-    for(cssStyle in styles) {
-        this.style[cssStyle] = styles[cssStyle];
+Object.prototype.addMethods = function(methods) {
+    for(var method in methods) {
+        this.prototype[method] = methods[method];
     }
     return this;
 };
 
-var $ = function(selector) {
-    if(typeof document.querySelector === 'function') {
-        var all = document.querySelectorAll(selector);
-        if(all != null && all.length > 1) {
-            return all;
-        } else {
-            return document.querySelector(selector);
+Object.addMethods({
+    isArray: function(object) {
+        return (object != null && typeof object == "object" && 'splice' in object && 'join' in object);
+    },
+    toQueryString: function() {
+        var params = []
+        for(key in this) {
+            var str = '';
+            if(typeof this[key] != 'function') {
+                str = encodeURIComponent(key)+'=';
+                var value = (Object.isArray(this[key])) ? this[key].join(',') : this[key];
+                str += encodeURIComponent(value);
+                params.push(str)
+            }
         }
-    } else {
-        //var cascade = selector.split(' ');
-        //var context;
-        //
-        //for(var i = 0; i < cascade.length; i++) {
-        //    if(cascade[i].indexOf('#') != -1) {
-        //        context = document.getElementById(cascade[i].split('#')[1]);
-        //    }
-        //}
-        //
-        //if((/^\w/).test(selector)) {
-        //    return document.getElementById(selector);
-        //} else if((/^\./).test(selector)) {
-        //    
-        //}
-        //if(selector.match(/^[ A-Za-z0-9]+/)) {
-        //    if(!document.getElementById(selector)) {
-        //        return document.querySelector(selector);
-        //    } else {
-        //        return document.getElementById(selector);
-        //    }
-        //} else {
-        //    return document.querySelector(selector);
-        //}
+        return params.join('&');
     }
-};
+});
 
-// jacked from prototype
-Object.extend = function(destination, source) {
-  for (var property in source)
-    destination[property] = source[property];
-  return destination;
-};
-
-
-//return this.map(function(pair) {
-//  var key = encodeURIComponent(pair.key), values = pair.value;
-//
-//  if (values && typeof values == 'object') {
-//    if (Object.isArray(values))
-//      return values.map(toQueryPair.curry(key)).join('&');
-//  }
-//  return toQueryPair(key, values);
-//}).join('&');
-
-Object.prototype.isArray = function(object) {
-    return (object != null && typeof object == "object" && 'splice' in object && 'join' in object);
-};
-
-Object.prototype.toQueryString = function() {
-    var params = []
-    for(key in this) {
-        var str = '';
-        if(typeof this[key] != 'function') {
-            str = encodeURIComponent(key)+'=';
-            var value = (Object.isArray(this[key])) ? this[key].join(',') : this[key];
-            str += encodeURIComponent(value);
-            params.push(str)
+Function.addMethods({
+    bindAsEventListener: function() {
+        var __method = this, object = arguments[0];
+        return function(event) {
+            return __method.apply(object, [event || window.event].concat(arguments));
+        }
+    },
+    bind: function() {
+        if (arguments.length < 2 && !arguments[0]) return this;
+        var __method = this,  object = arguments[0];
+        return function() {
+            return __method.apply(object, arguments);
         }
     }
-    return params.join('&');
-};
-
-/*
-Function.prototype.curry = function() {
-    if (!arguments.length) return this;
-    var __method = this;
-    return function() {
-      return __method.apply(this, arguments);
-    }
-};
-Function.prototype.delay = function() {
-    var __method = this, args = arguments, timeout = args[0] * 1000;
-    return window.setTimeout(function() {
-      return __method.apply(__method, args);
-    }, timeout);
-};
-Function.prototype.defer = Function.prototype.delay.curry(0.01);
-*/
+});
 
 var Ajax = function(options) {
     this.options = {
@@ -157,119 +99,129 @@ Ajax.Request = function(url, options) {
     } catch(e) {
         console.error('request error', e)
     }
-    
 };
-Ajax.Request.Events = ['Uninitialized', 'Connected', 'Requested', 'Processing', 'Complete', 'Failure', 'Success'];
+Object.extend(Ajax.Request, {
+    Events: ['Uninitialized', 'Connected', 'Requested', 'Processing', 'Complete', 'Failure', 'Success']
+});
 
-Ajax.Request.prototype.onStateChange = function() {
-    var readyState = this.transport.readyState;
-    if (readyState > 1 && !((readyState == 4) && this._complete)) {
-        this.respondToReadyState(this.transport.readyState);
-    }
-};
+Ajax.Request.addMethods({
+    onStateChange: function() {
+        var readyState = this.transport.readyState;
+        if (readyState > 1 && !((readyState == 4) && this._complete)) {
+            this.respondToReadyState(this.transport.readyState);
+        }
+    },
+    respondToReadyState: function(state) {
+        this._complete = (state === 4) ? true : false;
 
-Ajax.Request.prototype.respondToReadyState = function(state) {
-    this._complete = (state === 4) ? true : false;
-	
-	if(this._complete) {
-    	var response = new Ajax.Response(this.transport, this.options.format, this.options.sanitizeJSON)
+    	if(this._complete) {
+        	var response = new Ajax.Response(this.transport, this.options.format, this.options.sanitizeJSON)
 
-    	// if complete, check for onFailure or onSuccess and fire function if available
-        if(this.options.onFailure || this.options.onSuccess) {
+        	// if complete, check for onFailure or onSuccess and fire function if available
+            if(this.options.onFailure || this.options.onSuccess) {
+                try {
+                    (this.options['on'+Ajax.Request.Events[this.getSuccessCode()]] || function() {} )(response);
+                } catch(e) { /* fail silently */ }
+            }
+
+        	// if onEvent function is available for this state
             try {
-                (this.options['on'+Ajax.Request.Events[this.getSuccessCode()]] || function() {} )(response);
+        	    (this.options['on'+Ajax.Request.Events[state]] || function() {})(response);
             } catch(e) { /* fail silently */ }
+
+            // kill that object. we don't need it.
+            delete response;
+    	} else {
+    	    try {
+    			(this.options['on'+Ajax.Request.Events[state]] || function() {})();
+    	    } catch(e) { /* fail silently */ }
+    	}
+    },
+    getSuccessCode: function() {
+        var successCode = (!this.transport.status) ? 5 : (this.transport.status >= 200 && this.transport.status < 300) ? 6 : 5;
+        return successCode;
+    },
+    setRequestHeaders: function() {
+        var headers = {
+          'X-Requested-With': 'XMLHttpRequest',
+          'Accept': 'text/javascript, text/html, application/xml, text/xml, */*'
+        };
+
+        if(this.options.method == 'post') {
+            headers['Content-type'] = this.options.contentType+(this.options.encoding ? '; charset='+this.options.encoding : '');
+            /* Force "Connection: close" for older Mozilla browsers to work
+             * around a bug where XMLHttpRequest sends an incorrect
+             * Content-length header. See Mozilla Bugzilla #246651.
+             */
+            //headers['Content-length'] = this.options.params.length;
+            //headers['Connection'] = 'close';
         }
 
-    	// if onEvent function is available for this state
-        try {
-    	    (this.options['on'+Ajax.Request.Events[state]] || function() {})(response);
-        } catch(e) { /* fail silently */ }
-        
-        // kill that object. we don't need it.
-        delete response;
-	} else {
-	    try {
-			(this.options['on'+Ajax.Request.Events[state]] || function() {})();
-	    } catch(e) { /* fail silently */ }
-	}
-};
-
-Ajax.Request.prototype.getSuccessCode = function() {
-    var successCode = (!this.transport.status) ? 5 : (this.transport.status >= 200 && this.transport.status < 300) ? 6 : 5;
-    return successCode;
-};
-
-Ajax.Request.prototype.setRequestHeaders = function() {
-    var headers = {
-      'X-Requested-With': 'XMLHttpRequest',
-      'Accept': 'text/javascript, text/html, application/xml, text/xml, */*'
-    };
-    
-    if(this.options.method == 'post') {
-        headers['Content-type'] = this.options.contentType+(this.options.encoding ? '; charset='+this.options.encoding : '');
-        headers['Content-length'] = this.options.params.length;
-        /* Force "Connection: close" for older Mozilla browsers to work
-         * around a bug where XMLHttpRequest sends an incorrect
-         * Content-length header. See Mozilla Bugzilla #246651.
-         */
-        //headers['Connection'] = 'close';
-    }
-    
-    for(var name in headers) {
-        if(typeof headers[name] != 'function') {
-            this.transport.setRequestHeader(name, headers[name]);
+        for(var name in headers) {
+            if(typeof headers[name] != 'function') {
+                this.transport.setRequestHeader(name, headers[name]);
+            }
         }
     }
-};
+});
 
 Ajax.Response = function(response, format, sanitize) {
 	this.response = response;
 	this.format = format;
 	this.sanitizeJSON = sanitize;
-    
+
     try {
     	return this.getResponse();
     } catch(e) {
         return null;
     }
 };
+Ajax.Response.addMethods({
+    getResponse: function() {
+    	switch(this.format.toLowerCase()) {
+    		case 'xml':
+    		    var xml = (this.response.responseXML) ? this.response.responseXML : new String('');
+    			return xml;
+    		break;
+    		case 'json':
+    			return this.evalJSON();
+    		break;
+    		case 'object':
+    			return eval('('+this.response.responseText+')');
+    		break;
+    		case 'text':
+    		    return this.response.responseText;
+    		break
+    		default: 
+    			return this.response;
+    		break;
+    	}
+    },
+    isJSON: function() {
+    	str = this.response.responseText.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, '');
+    	return (/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(str);
+    },
+    evalJSON: function() {
+    	var json = this.response.responseText;
+    	if(this.sanitizeJSON) { json = json.sub(/^\/\*-secure-([\s\S]*)\*\/\s*$/, '#{1}'); }
 
-Ajax.Response.prototype.getResponse = function() {
-	switch(this.format.toLowerCase()) {
-		case 'xml':
-		    var xml = (this.response.responseXML) ? this.response.responseXML : new String('');
-			return xml;
-		break;
-		case 'json':
-			return this.evalJSON();
-		break;
-		case 'object':
-			return eval('('+this.response.responseText+')');
-		break;
-		case 'text':
-		    return this.response.responseText;
-		break
-		default: 
-			return this.response;
-		break;
-	}
+    	try {
+    		if(this.isJSON(json)) {
+    			return eval('('+json+')');
+    		}
+    	} catch(e) {
+    		console.error('json eval error', e);
+    	}
+    }
+});
+
+var Element = function(type, atts) {
+    this.el = document.createElement(type);
+
+    for(attr in atts) {
+        this.el.setAttribute(attr, atts[attr]);
+    }
+    
+    return this.el;
 };
 
-Ajax.Response.prototype.isJSON = function() {
-	str = this.response.responseText.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, '');
-	return (/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(str);
-};
-
-Ajax.Response.prototype.evalJSON = function() {
-	var json = this.response.responseText;
-	if(this.sanitizeJSON) { json = json.sub(/^\/\*-secure-([\s\S]*)\*\/\s*$/, '#{1}'); }
-
-	try {
-		if(this.isJSON(json)) {
-			return eval('('+json+')');
-		}
-	} catch(e) {
-		console.error('json eval error', e);
-	}
-};
